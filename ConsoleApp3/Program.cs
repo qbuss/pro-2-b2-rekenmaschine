6f9b67081388a001cb494579eaf07427cc53b720
qbuss <kuba.farionn@gmail.com>
2019-11-21 12:19:35 +0100

    klaar

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -8,27 +8,82 @@ namespace ConsoleApp3
         {
             int nummer1 = 0;
             int nummer2 = 0;
-            
-            while(true)
+            int resultaat = 0;
+
+
+            while (true)
             {
                 //eerste
                 header();
-                Console.WriteLine("schrijf nu je eerste letter");
+                Console.WriteLine("schrijf nu je eerste cijfer");
                 nummer1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+                //derde
+                header();
+                Console.WriteLine("schrijf nu je tweede cijfer");
+                nummer2 = Convert.ToInt32(Console.ReadLine());
 
                 //tweede
                 header();
-                Console.WriteLine("modulo = %, min = -, plus = +");
+                Console.WriteLine("================");
+                Console.WriteLine("modulo");
+                Console.WriteLine("min");
+                Console.WriteLine("plus");
+                Console.WriteLine("================");
                 Console.WriteLine("schrijf nu je functie");
-                nummer1 = Convert.ToInt32(Console.ReadLine());
-                //derde
+                switch (Console.ReadLine())
+                {
+                    case "plus":
+                        resultaat = plus(nummer1, nummer2);
+                        break;
+                    case "min":
+                        resultaat = min(nummer1, nummer2);
+                        break;
+                    case "modulo":
+                        resultaat = modulo(nummer1, nummer2);
+                        break;
+                    case "delen":
+                        resultaat = delen(nummer1, nummer2);
+                        break;
+                    case "keer":
+                        resultaat = keer(nummer1, nummer2);
+                        break;
+                }
+                Console.WriteLine("Jouw resultaat is " + resultaat);
+                Console.WriteLine("==================================");
+                Console.WriteLine("Click een knopje om te continueren");
+                Console.ReadKey();
 
+                
+            }
+
+            int plus(int int1, int int2)
+            {
+                return int1 + int2;
+            }
+            int min(int int1, int int2)
+            {
+                return int1 - int2;
+            }
+            int modulo(int int1, int int2)
+            {
+                return int1 % int2;
+            }
+            int delen(int int1, int int2)
+            {
+                return int1 / int2;
+            }
+            int keer(int int1, int int2)
+            {
+                return int1 * int2;
             }
             void header()
             {
                 Console.Clear();
                 Console.WriteLine("--==========================--");
-                Console.WriteLine("--==Welkom in rekenmachine==--"); 
+                Console.WriteLine("--==Welkom in rekenmachine==--");
                 Console.WriteLine("--==========================--");
             }
         }
